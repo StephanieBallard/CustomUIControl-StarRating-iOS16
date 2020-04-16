6d9edeb1a3a3f5b7ea7888c42ab993f5eb3b5839
Stephanie Ballard <user.name>
2020-04-16 15:25:28 -0700

    folders and files set up

---
+++ CustomUIControl-StarRating/Control/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  CustomUIControl-StarRating
+//
+//  Created by Stephanie Ballard on 4/16/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    var value = 1
+}
---
+++ CustomUIControl-StarRating/View/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CustomUIControl-StarRating
+//
+//  Created by Stephanie Ballard on 4/16/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
